83187509df65be48044a2bfadcdef3d5e9b8955a
Rostishkin <pylypyakrostyslav@gmail.com>
2023-06-22 14:37:45 +0200

    Header.css + Header.js
    Mise en place Header.css + header.js

--- kasa/src/components/Header.js
+++ kasa/src/components/Header.js
@@ -1,13 +1,21 @@
 import {Link} from "react-router-dom";
+import logo from '../assets/logo.svg';
+import '../style/Header.css';
 
 export default function Header() {
     return (
-        <div>
-            <p>Kasa</p>
-            <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/a-propos">À Propos</Link>
+        <header className="header">
+            <Link to="/">
+                <img src={logo} alt="Logo" />
+            </Link>
+            <nav className="nav">
+                <Link to="/" className={`navigation-link ${location.pathname === '/' ? 'active-link' : ''}`}>
+                    Accueil
+                </Link>
+                <Link to="/a-propos-de-nous" className={`navigation-link ${location.pathname === '/a-propos-de-nous' ? 'active-link': ''}`}>
+                    A Propos
+                </Link>
             </nav>
-        </div>
-    )
+        </header>
+    );
 }

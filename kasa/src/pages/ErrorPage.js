e1ae4cef33421508a9fc3d6da50c94672141ccee
Rostishkin <pylypyakrostyslav@gmail.com>
2023-06-29 21:32:23 +0200

    Errorpage Css + responsive

--- kasa/src/pages/ErrorPage.js
+++ kasa/src/pages/ErrorPage.js
@@ -4,11 +4,13 @@ import '../style/ErrorPage.css';
 export default function ErrorPage () {
     return (
     <main>
-    <h1 className='error'>404</h1>
-    <h2 className='error-message'>Oups! La page que vous demandez n'existe pas.</h2>
-    <Link to={`/`} className='comeback-message'>
-    Retourner sur la page d’accueil
-    </Link>        
-</main>
+        <div className='error'>
+        <h1 className='error-titre'>404</h1>
+        <h2 className='error-message'>Oups! La page que vous demandez n'existe pas.</h2>
+        <Link to={`/`} className='error-comeback-message'>
+        Retourner sur la page d’accueil
+        </Link>
+        </div>
+    </main>
     )
 }

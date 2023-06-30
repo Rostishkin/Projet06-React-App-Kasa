b6164f945e2a58379b5aa14ec41b92ad234836e8
Rostishkin <pylypyakrostyslav@gmail.com>
2023-06-30 13:56:01 +0200

    Animation Collapse

--- kasa/src/components/Collapse.js
+++ kasa/src/components/Collapse.js
@@ -7,25 +7,29 @@ import { faChevronUp} from '@fortawesome/free-solid-svg-icons'; //import de chev
 export default function Collapse({ title, text, customClasses}) {
     const [isOpen, setIsOpen] = useState(true);
 
+    const [iconRotation, setIconRotation] = useState(0);
+
     const toggleCollapse = () => {
         setIsOpen(!isOpen);
-    };
+        setIconRotation(iconRotation === 0 ? 180 : 0);
+    }
 
     return (
         <div className={`collapse ${customClasses}`}>
             <div className={`collapse-container ${customClasses}`}>
             <button className={`collapse-header ${customClasses}`} onClick={toggleCollapse}>
-                <h3 className={`collapse-title ${customClasses}`}>{title}</h3>
-                <FontAwesomeIcon
+                    <h3 className={`collapse-title ${customClasses}`}>{title}</h3>
+                    <FontAwesomeIcon
                     icon={faChevronUp}
-                    className={`collapse-icon ${isOpen ?'':'is-open'}`}
-                />
-            </button>
-            {!isOpen &&
-                <div className={`collapse-body ${customClasses}${isOpen ? '' : 'is-open'}`}>
+                    className={`collapse-icon ${isOpen ? '' : 'is-open'}`}
+                    style={{ transform: `rotate(${iconRotation}deg)` }}
+                     />
+                    </button>
+                    {!isOpen &&
+                    <div className={`collapse-body ${customClasses}${isOpen ? '' : 'is-open'}`}>
                     {text}
-                </div>
-            }
+                    </div>
+                    }
             </div>
         </div>
     );

97a6359d23ea7954af382791663d6b1d2ea59f74
Rostishkin <112947430+Rostishkin@users.noreply.github.com>
2023-07-03 13:29:24 +0200

    Update Footer.js

--- kasa/src/components/Footer.js
+++ kasa/src/components/Footer.js
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom';
-import logo from "../assets/logo-FOOTER.svg"
+import logo from "../assets/logo-footer.svg"
 import '../style/Footer.css'
 
 export default function Footer() {
